eef34d6c7511e079607654cc5bbcf1d0f7fe68f6
JunYu0930 <4b1g0015@stust.edu.tw>
2023-11-24 10:57:22 +0800

    Create 1124.py
    create class student

---
+++ 1124.py
@@ -0,0 +1,40 @@
+class Course:
+    def __init__(self, course_name,course_id, course_credits):
+        self.course_name = course_name
+        self.course_id = course_id
+        self.course_credits = course_credits
+class Student:
+    def __init__(self, stu_name, stu_id):
+        self.stu_name = stu_name
+        self.stu_id = stu_id
+        
+    def __str__(self):
+        return f"Name: {self.stu_name}\nID = {self.stu_id}"
+
+    
+    def addCourse(self, object):
+        self.course_info = object
+
+c1 = Course(course_name="套裝軟體應用",
+            course_id="G0D17M01",
+            course_credits=3)
+s1 = Student(stu_name="鐘竣羽",stu_id="4B1G0015")
+
+
+
+
+class Person:
+    def __init__(self, name_, age_):
+        self.name = name_
+        self.age = age_
+    
+    def __str__(self):
+        return f"Name: {self.name}\nAge = {self.age}"
+    
+p1 = Person("John", 36)
+print(p1)
+
+p2 = Person("Sue", 22)
+print(p2.__dict__)
+
+
